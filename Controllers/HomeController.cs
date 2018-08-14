2d8fff5b53a7bcb486dd38dbd7cbc3ae03a60c61
Ondrej Tadanai <otadanai@specpage.com>
2018-08-14 10:58:16 +0200

    removed testapp.tests.csproj

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,34 +1,25 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using TestApp.Services;
 using TestApp.ViewModel;
 
 namespace TestApp.Controllers
 {
-	public class HomeController : Controller
+    public class HomeController : Controller
     {
-		private readonly IDbManager _dbManager;
-
-		public HomeController(IDbManager dbManager)
+        public HomeController(IDbManager dbManager)
 		{
-			_dbManager = dbManager;
 		}
 
         public IActionResult Index()
         {
             return View();
-	        if (!_dbManager.IsConfigurationComplete())
-			{
-				return RedirectToAction("Index", "Configuration");
-			}
-            return Redirect("/jobs");
         }
 
         public IActionResult Error()
         {
 			return View(new ErrorViewModel
 			{
-				RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+				RequestId = HttpContext.TraceIdentifier
 			});
         }
     }
